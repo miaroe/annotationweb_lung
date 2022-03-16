eca8c8962626bb9a7e1d8730166e2e56d3c27953
ingridtv <ingridtveten@hotmail.com>
2022-03-16 16:18:54 +0100

    Keep only PatientXX/SequenceXX/frameX.png in task image list

--- annotationweb/templatetags/get_sequence_name.py
+++ annotationweb/templatetags/get_sequence_name.py
@@ -10,3 +10,24 @@ def get_sequence_name(image_sequence_format):
     path = os.path.dirname(image_sequence_format)
     sequence_dir = os.path.basename(path)
     return sequence_dir
+
+
+@register.filter
+def get_sequence_and_filename(image_sequence_format):
+    """
+    Get sequence folder name and filename from an ImageSequence format string
+    """
+    path, fn = os.path.split(image_sequence_format)
+    sequence_dir = os.path.join(os.path.basename(path), fn)
+    return sequence_dir
+
+
+@register.filter
+def get_patient_sequence_and_filename(image_sequence_format):
+    """
+    Get patient_xx/sequence_xx/filename.png from an ImageSequence format string
+    """
+    seq_path, fn = os.path.split(image_sequence_format)
+    patient_path, seq_dir = os.path.split(seq_path)
+    path = os.path.join(os.path.basename(patient_path), seq_dir, fn)
+    return path
